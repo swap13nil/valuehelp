854837ca5ad833edb6a0924b8b843973f681f61d
Swapnil Palande <swap13nil@gmail.com>
2019-11-01 08:58:57 +0000

    3 rd push

--- webapp/controller/help.controller.js
+++ webapp/controller/help.controller.js
@@ -17,55 +17,30 @@ sap.ui.define([
 		},
 
 		onValueHelpRequested: function() {
-			var aCols = this.oColModel.getData().cols;
-
-			this._oValueHelpDialog = sap.ui.xmlfragment("value.valueHelp.view.valueHelp", this);
-			this.getView().addDependent(this._oValueHelpDialog);
-
+		
+        	
+        		var odiloge1 = sap.ui.xmlfragment("value.valueHelp.view.quantity", this);
+        		this.getView().addDependent(odiloge1);
+			   
+			     odiloge1.bindAggregation("items" , {
+			     	path:"product>/ProductCollection",
+			     	template : new sap.m.StandardListItem({
+			     		
+			     		title:"{product>ProductId}"
+			     	})
+			     });
 			
-			this._oValueHelpDialog.getTableAsync().then(function (oTable) {
-				oTable.setModel(this.oProductsModel);
-				oTable.setModel(this.oColModel, "columns");
-
-				if (oTable.bindRows) {
-					oTable.bindAggregation("rows", "/ProductCollection");
-				}
-
-				if (oTable.bindItems) {
-					oTable.bindAggregation("items", "/ProductCollection", function () {
-						return new ColumnListItem({
-							cells: aCols.map(function (column) {
-								return new Label({ text: "{" + column.template + "}" });
-							})
-						});
-					});
-				}
-
-				this._oValueHelpDialog.update();
-			}.bind(this));
-
-			var oToken = new Token();
-			oToken.setKey(this._oInput.getSelectedKey());
-			oToken.setText(this._oInput.getValue());
-			this._oValueHelpDialog.setTokens([oToken]);
-			this._oValueHelpDialog.open();
-		},
+  odiloge1.open();
+        },
 
 		onValueHelpOkPress: function (oEvent) {
 	   
-			var aTokens = oEvent.getParameter("tokens");
-			this._oInput.setSelectedKey(aTokens[0].getKey());
-			this._oValueHelpDialog.close();
+		
+	
 	
 		},
 
-		onValueHelpCancelPress: function () {
-			this._oValueHelpDialog.close();
-		},
-
-		onValueHelpAfterClose: function () {
-			this._oValueHelpDialog.destroy();
-		},
+	
         
         
         
